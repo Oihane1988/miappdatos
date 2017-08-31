fab70e8bd38f8cc9a4bc7dba1c4f1f36edd90fa8
Oihane <oburuaga@gmail.com>
2017-08-31 11:34:46 +0200

    Congelamos la app

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
-
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+csv_path = './static/incendio.csv'
+csv_obj = csv.DictReader(open(csv_path, 'r'))
+csv_list = list(csv_obj)
+
 @app.route("/")
 def index():
-return render_template('index.html')
-							
+    return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0",port=8000,use_reloader=True,debug=True,)
